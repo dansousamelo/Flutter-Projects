5f8001c23c59544726c3bde6fabb9eba5b7f2d21
Daniel Sousa <daniel_sousa.unb@outlook.com>
2019-07-23 09:23:32 -0300

    Adicionando FutureBuilder

--- conversao_de_moeda_app/lib/main.dart
+++ conversao_de_moeda_app/lib/main.dart
@@ -3,18 +3,17 @@ import 'dart:async';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-//const request = "https://api.hgbrasil.com/finance?key=eab1a881";
+const request = "https://api.hgbrasil.com/finance?key=eab1a881";
 
-void main() async{
-
+void main() async {
   runApp(MaterialApp(
     home: Home(),
   ));
 }
 
-//Future<Map> getData() async {
-//  http.Response response = await http.get(request);
-//  return json.decode(response.body);
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
 }
 
 class Home extends StatefulWidget {
@@ -32,9 +31,35 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.amber,
         centerTitle: true,
       ),
-
+      body: FutureBuilder<Map>(
+          future: getData(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return Center(
+                  child: Text(
+                    "Carregando Dados ...",
+                    style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              default:
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text(
+                      "Carregando Dados ...",
+                      style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                      textAlign: TextAlign.center,
+                    ),
+                  );
+                } else {
+                  return Container(
+                    color: Colors.green,
+                  );
+                }
+            }
+          }),
     );
   }
 }
-
-
